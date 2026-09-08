2ce6aef83be35a5f1fabdc2bae4f3eddafe56b50
aditya2206-afk <adityapatil3260@gmail.com>
2026-09-08 22:22:46 +0530

    data seed fro the rider717

--- backend/prisma/seedPremiumRates.ts
+++ backend/prisma/seedPremiumRates.ts
@@ -8,6 +8,7 @@ import { seedPlan912PremiumRates } from "./seedPremiumRate912";
 import { seedPremiumRates887 } from "./seedPremiumRates887";
 import { seedRiderPremium714 } from "./seedRider714Rates";
 import { seedRiderPremium715 } from "./seedRider715Rates";
+import { seedRiderPremium717 } from "./seedRider717Rates";
 import { seedRiderPremiumCIR } from "./seedRiderCIR";
 import { seedPPT714 } from "./seedPPT714";
 import { seedPPT715 } from "./seedPPT715";
@@ -39,6 +40,7 @@ export const seedPremiumRates = async (prisma: PrismaClient) => {
   await seedPremiumRates887(prisma);
   await seedRiderPremium714(prisma);
   await seedRiderPremium715(prisma);
+  await seedRiderPremium717(prisma);
   await seedRiderPremiumCIR(prisma);
   await seedPPT714(prisma);
   await seedPPT715(prisma);
